754932525433658562fa94ec84b4933f6fef3231
Kacper Barwinski <barwinski@student.agh.edu.pl>
2022-06-13 17:26:11 +0200

    db

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,16 +2,41 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-//@Data we dont want final :(
+//@Data wiec bez final
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String name;
+
     private String email;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
+
+
